e4f1ed33007a16a783bfb2e05102e4be8b1506f5
Connor McG <c.mcg132@gmail.com>
2018-12-09 18:40:50 +0000

    Removed getMessage and getMessageHandler

--- index.js
+++ index.js
@@ -2,6 +2,4 @@ var api = require('./dist/api.bundle').validation;
 
 exports.setMessageHandlers = api.setMessageHandlers;
 exports.setMessageHandler = api.setMessageHandler;
-exports.getMessage = api.getMessage;
-exports.getMessageHandler = api.getMessageHandler;
 exports.validateForm = api.validateForm;
