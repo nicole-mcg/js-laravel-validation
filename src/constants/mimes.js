77a09e2e3dc793f264fa3963f57eef7fd81b6f64
Connor <c.mcg132@gmail.com>
2018-12-09 20:50:58 -0500

    Changed mimes.js format

--- src/constants/mimes.js
+++ src/constants/mimes.js
@@ -1,220 +1,94 @@
 export default {
-    "text/html": [
-        "html",
-        "htm",
-        "shtml"
-    ],
-    "text/css": [
-        "css"
-    ],
-    "text/xml": [
-        "xml"
-    ],
-    "image/gif": [
-        "gif"
-    ],
-    "image/jpeg": [
-        "jpeg",
-        "jpg"
-    ],
-    "application/x-javascript": [
-        "js"
-    ],
-    "application/atom+xml": [
-        "atom"
-    ],
-    "application/rss+xml": [
-        "rss"
-    ],
-    "text/mathml": [
-        "mml"
-    ],
-    "text/plain": [
-        "txt"
-    ],
-    "text/vnd.sun.j2me.app-descriptor": [
-        "jad"
-    ],
-    "text/vnd.wap.wml": [
-        "wml"
-    ],
-    "text/x-component": [
-        "htc"
-    ],
-    "image/png": [
-        "png"
-    ],
-    "image/tiff": [
-        "tif",
-        "tiff"
-    ],
-    "image/vnd.wap.wbmp": [
-        "wbmp"
-    ],
-    "image/x-icon": [
-        "ico"
-    ],
-    "image/x-jng": [
-        "jng"
-    ],
-    "image/x-ms-bmp": [
-        "bmp"
-    ],
-    "image/svg+xml": [
-        "svg"
-    ],
-    "image/webp": [
-        "webp"
-    ],
-    "application/java-archive": [
-        "jar",
-        "war",
-        "ear"
-    ],
-    "application/mac-binhex40": [
-        "hqx"
-    ],
-    "application/msword": [
-        "doc"
-    ],
-    "application/pdf": [
-        "pdf"
-    ],
-    "application/postscript": [
-        "ps",
-        "eps",
-        "ai"
-    ],
-    "application/rtf": [
-        "rtf"
-    ],
-    "application/vnd.ms-excel": [
-        "xls"
-    ],
-    "application/vnd.ms-powerpoint": [
-        "ppt"
-    ],
-    "application/vnd.wap.wmlc": [
-        "wmlc"
-    ],
-    "application/vnd.google-earth.kml+xml": [
-        "kml"
-    ],
-    "application/vnd.google-earth.kmz": [
-        "kmz"
-    ],
-    "application/x-7z-compressed": [
-        "7z"
-    ],
-    "application/x-cocoa": [
-        "cco"
-    ],
-    "application/x-java-archive-diff": [
-        "jardiff"
-    ],
-    "application/x-java-jnlp-file": [
-        "jnlp"
-    ],
-    "application/x-makeself": [
-        "run"
-    ],
-    "application/x-perl": [
-        "pl",
-        "pm"
-    ],
-    "application/x-pilot": [
-        "prc",
-        "pdb"
-    ],
-    "application/x-rar-compressed": [
-        "rar"
-    ],
-    "application/x-redhat-package-manager": [
-        "rpm"
-    ],
-    "application/x-sea": [
-        "sea"
-    ],
-    "application/x-shockwave-flash": [
-        "swf"
-    ],
-    "application/x-stuffit": [
-        "sit"
-    ],
-    "application/x-tcl": [
-        "tcl",
-        "tk"
-    ],
-    "application/x-x509-ca-cert": [
-        "der",
-        "pem",
-        "crt"
-    ],
-    "application/x-xpinstall": [
-        "xpi"
-    ],
-    "application/xhtml+xml": [
-        "xhtml"
-    ],
-    "application/zip": [
-        "zip"
-    ],
-    "application/octet-stream": [
-        "bin",
-        "exe",
-        "dll",
-        "deb",
-        "dmg",
-        "eot",
-        "iso",
-        "img",
-        "msi",
-        "msp",
-        "msm"
-    ],
-    "audio/midi": [
-        "mid",
-        "midi",
-        "kar"
-    ],
-    "audio/mpeg": [
-        "mp3"
-    ],
-    "audio/ogg": [
-        "ogg"
-    ],
-    "audio/x-realaudio": [
-        "ra"
-    ],
-    "video/3gpp": [
-        "3gpp",
-        "3gp"
-    ],
-    "video/mpeg": [
-        "mpeg",
-        "mpg"
-    ],
-    "video/quicktime": [
-        "mov"
-    ],
-    "video/x-flv": [
-        "flv"
-    ],
-    "video/x-mng": [
-        "mng"
-    ],
-    "video/x-ms-asf": [
-        "asx",
-        "asf"
-    ],
-    "video/x-ms-wmv": [
-        "wmv"
-    ],
-    "video/x-msvideo": [
-        "avi"
-    ],
-    "video/mp4": [
-        "m4v",
-        "mp4"
-    ],
+        "html": "text/html",
+        "htm": "text/html",
+        "shtml": "text/html",
+        "css": "text/css",
+        "xml": "text/xml",
+        "gif": "image/gif",
+        "jpeg": "image/jpeg",
+        "jpg": "image/jpeg",
+        "js": "application/x-javascript",
+        "atom": "application/atom+xml",
+        "rss": "application/rss+xml",
+        "mml": "text/mathml",
+        "txt": "text/plain",
+        "jad": "text/vnd.sun.j2me.app-descriptor",
+        "wml": "text/vnd.wap.wml",
+        "htc": "text/x-component",
+        "png": "image/png",
+        "tif": "image/tiff",
+        "tiff": "image/tiff",
+        "wbmp": "image/vnd.wap.wbmp",
+        "ico": "image/x-icon",
+        "jng": "image/x-jng",
+        "bmp": "image/x-ms-bmp",
+        "svg": "image/svg+xml",
+        "webp": "image/webp",
+        "jar": "application/java-archive",
+        "war": "application/java-archive",
+        "ear": "application/java-archive",
+        "hqx": "application/mac-binhex40",
+        "doc": "application/msword",
+        "pdf": "application/pdf",
+        "ps": "application/postscript",
+        "eps": "application/postscript",
+        "ai": "application/postscript",
+        "rtf": "application/rtf",
+        "xls": "application/vnd.ms-excel",
+        "ppt": "application/vnd.ms-powerpoint",
+        "wmlc": "application/vnd.wap.wmlc",
+        "kml": "application/vnd.google-earth.kml+xml",
+        "kmz": "application/vnd.google-earth.kmz",
+        "7z": "application/x-7z-compressed",
+        "cco": "application/x-cocoa",
+        "jardiff": "application/x-java-archive-diff",
+        "jnlp": "application/x-java-jnlp-file",
+        "run": "application/x-makeself",
+        "pl": "application/x-perl",
+        "pm": "application/x-perl",
+        "prc": "application/x-pilot",
+        "pdb": "application/x-pilot",
+        "rar": "application/x-rar-compressed",
+        "rpm": "application/x-redhat-package-manager",
+        "sea": "application/x-sea",
+        "swf": "application/x-shockwave-flash",
+        "sit": "application/x-stuffit",
+        "tcl": "application/x-tcl",
+        "tk": "application/x-tcl",
+        "der": "application/x-x509-ca-cert",
+        "pem": "application/x-x509-ca-cert",
+        "crt": "application/x-x509-ca-cert",
+        "xpi": "application/x-xpinstall",
+        "xhtml": "application/xhtml+xml",
+        "zip": "application/zip",
+        "bin": "application/octet-stream",
+        "exe": "application/octet-stream",
+        "dll": "application/octet-stream",
+        "deb": "application/octet-stream",
+        "dmg": "application/octet-stream",
+        "eot": "application/octet-stream",
+        "iso": "application/octet-stream",
+        "img": "application/octet-stream",
+        "msi": "application/octet-stream",
+        "msp": "application/octet-stream",
+        "msm": "application/octet-stream",
+        "mid": "audio/midi",
+        "midi": "audio/midi",
+        "kar": "audio/midi",
+        "mp3": "audio/mpeg",
+        "ogg": "audio/ogg",
+        "ra": "audio/x-realaudio",
+        "3gpp": "video/3gpp",
+        "3gp": "video/3gpp",
+        "mpeg": "video/mpeg",
+        "mpg": "video/mpeg",
+        "mov": "video/quicktime",
+        "flv": "video/x-flv",
+        "mng": "video/x-mng",
+        "asx": "video/x-ms-asf",
+        "asf": "video/x-ms-asf",
+        "wmv": "video/x-ms-wmv",
+        "avi": "video/x-msvideo",
+        "m4v": "video/mp4",
+        "mp4": "video/mp4",
 }
