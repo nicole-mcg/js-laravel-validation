c2bf45b23d799baebb2787d44a4fad4828a37255
Nik <niknakbakpak@gmail.com>
2019-02-20 07:47:31 -0500

    update build

--- dist/api.js
+++ dist/api.js
@@ -4,19 +4,11 @@ var _messages = require("./messages");
 
 var _index = require("./index");
 
-// var messages = require('./messages');
-// var validate = require('./index').validate;
-//console.log('api validate=', validate)
-// exports.setMessageHandler = messages.setMessageHandler;
-// exports.setMessageHandler = messages.setMessageHandler;
-// exports.getMessage = messages.getMessage;
-// exports.getMessageHandler = messages.getMessageHandler;
-// exports.validateForm = validate.validateForm;
 var validation = {
   setMessageHandlers: _messages.setMessageHandlers,
   setMessageHandler: _messages.setMessageHandler,
-  getMessage: getMessage,
-  getMessageHandler: getMessageHandler,
+  getMessage: _messages.getMessage,
+  getMessageHandler: _messages.getMessageHandler,
   validateForm: _index.validate.validateForm
 };
 exports.validation = validation;
--- src/api.js
+++ src/api.js
@@ -1,20 +1,11 @@
 import {
     setMessageHandlers,
     setMessageHandler,
+    getMessage,
+    getMessageHandler,
 } from './messages';
 
 import { validate } from './index';
-
-// var messages = require('./messages');
-// var validate = require('./index').validate;
-
-//console.log('api validate=', validate)
-
-// exports.setMessageHandler = messages.setMessageHandler;
-// exports.setMessageHandler = messages.setMessageHandler;
-// exports.getMessage = messages.getMessage;
-// exports.getMessageHandler = messages.getMessageHandler;
-// exports.validateForm = validate.validateForm;
 
 const validation = {
     setMessageHandlers,
